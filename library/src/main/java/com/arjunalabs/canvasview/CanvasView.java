041fc11992236bc39253b139c4607b51b1c3bc7c
Bobby Prabowo <bobbyadiprabowo@Bopbi-MacBook-Pro.local>
2016-11-12 21:59:44 +0700

    init repo

---
+++ library/src/main/java/com/arjunalabs/canvasview/CanvasView.java
@@ -0,0 +1,31 @@
+package com.arjunalabs.canvasview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bobbyadiprabowo on 11/12/16.
+ */
+
+public class CanvasView extends View {
+
+    public CanvasView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init() {
+
+    }
+}
